92e0b230994cd7a42d4e82d5fe4d01d4a34855b2
CaesiumY <mn0658@naver.com>
2020-05-19 13:16:59 +0900

    chore: Add antd

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
+import { Card } from "antd";
 
 export class LoginPage extends Component {
   render() {
-    return <div>login page</div>;
+    return (
+      <div>
+        <Card title="Default size card" style={{ width: 300 }}>
+          <p>Card content</p>
+          <p>Card content</p>
+          <p>Card content</p>
+        </Card>
+      </div>
+    );
   }
 }
 
