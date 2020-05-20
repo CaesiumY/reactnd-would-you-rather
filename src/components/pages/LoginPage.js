882f785e7e901ddc6eb218bab51edc8bc15df8c0
caesiumy <mn0658@naver.com>
2020-05-20 23:01:26 +0900

    feat: Add login page card frame

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
-import { Card } from "antd";
+import { Card, Button } from "antd";
+import { LoginOutlined } from "@ant-design/icons";
 
 export class LoginPage extends Component {
   render() {
     return (
       <div>
-        <Card title="Default size card" style={{ width: 300 }}>
+        <Card title="Login Page" style={{ textAlign: "center" }}>
+          <img src="/logo192.png" alt="logo" />
           <p>Card content</p>
           <p>Card content</p>
           <p>Card content</p>
+          <Button type="primary" block icon={<LoginOutlined />}>
+            Login
+          </Button>
         </Card>
       </div>
     );
