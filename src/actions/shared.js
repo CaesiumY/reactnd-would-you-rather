1b411fe9c1f821d4b76ed0cc203dbea73167e598
caesiumy <mn0658@naver.com>
2020-05-23 23:17:51 +0900

    test: Add default authedUser to jump login

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,12 +2,18 @@ import { getInitialData } from "../utils/api";
 import { receiveUsers } from "./users";
 import { receiveQuestions } from "./questions";
 import { showLoading, hideLoading } from "react-redux-loading-bar";
+import { setAuthedUser } from "./authedUser";
+
+// FIXME - after TEST, it should be fixed.
+const authedUser = "johndoe";
 
 export function hadleInitialData() {
   return (dispatch) => {
     dispatch(showLoading());
     return getInitialData().then(({ users, questions }) => {
       dispatch(receiveUsers(users));
+      // FIXME - after TEST, it should be fixed.
+      dispatch(setAuthedUser(authedUser));
       dispatch(receiveQuestions(questions));
       dispatch(hideLoading());
     });
