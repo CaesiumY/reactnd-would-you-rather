5bede86df444094e7624705cc28ecf715837b47e
caesiumy <mn0658@naver.com>
2020-05-24 00:09:25 +0900

    feat: Add mapStateToProps in LeaderBoard

--- src/components/pages/LeaderBoard.js
+++ src/components/pages/LeaderBoard.js
@@ -1,9 +1,39 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { TrophyTwoTone } from "@ant-design/icons";
 
 export class LeaderBoard extends Component {
+  state = {
+    colorSet: ["#fbbd08", "#767676", "#f2711c"],
+  };
+
   render() {
-    return <div>LeaderBoard</div>;
+    return (
+      <div>
+        <TrophyTwoTone
+          twoToneColor={this.state.colorSet[2]}
+          style={{ fontSize: "3rem" }}
+        />
+        LeaderBoard
+      </div>
+    );
   }
 }
 
-export default LeaderBoard;
+function mapStateToProps({ users }) {
+  const leaderBoardData = Object.values(users)
+    .map((user) => ({
+      id: user.id,
+      name: user.name,
+      avatarURL: user.avatarURL,
+      answerCount: Object.values(user.answers).length,
+      questionCount: user.questions.length,
+      totalCount: Object.values(user.answers).length + user.questions.length,
+    }))
+    .sort((a, b) => b.totalCount - a.totalCount);
+  return {
+    leaderBoardData,
+  };
+}
+
+export default connect(mapStateToProps)(LeaderBoard);
