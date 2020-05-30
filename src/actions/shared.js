5376730013f75556a5f2eda54eaba8e26ad89905
caesiumy <mn0658@naver.com>
2020-05-30 17:51:33 +0900

    chore: Remove default login

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,19 +2,19 @@ import { getInitialData } from "../utils/api";
 import { receiveUsers } from "./users";
 import { receiveQuestions } from "./questions";
 import { showLoading, hideLoading } from "react-redux-loading-bar";
-import { setAuthedUser } from "./authedUser";
+// import { setAuthedUser } from "./authedUser";
 
-// FIXME - after TEST, it should be fixed.
-const authedUser = "johndoe";
+// NOTE - below this comment makes not login is default
+// const authedUser = "johndoe";
 
 export function hadleInitialData() {
   return (dispatch) => {
     dispatch(showLoading());
     return getInitialData().then(({ users, questions }) => {
       dispatch(receiveUsers(users));
-      // FIXME - after TEST, it should be fixed.
       dispatch(receiveQuestions(questions));
-      dispatch(setAuthedUser(authedUser));
+      // NOTE - below this comment makes not login is default
+      // dispatch(setAuthedUser(authedUser));
       dispatch(hideLoading());
     });
   };
