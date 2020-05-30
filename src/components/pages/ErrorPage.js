da5c880dc0a5985b573062e5cb29bd10f419e5a2
caesiumy <mn0658@naver.com>
2020-05-30 17:44:58 +0900

    feat: Remove Back Home button

--- src/components/pages/ErrorPage.js
+++ src/components/pages/ErrorPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Result, Button } from "antd";
+import { Result } from "antd";
 
 function ErrorPage() {
   return (
@@ -7,7 +7,6 @@ function ErrorPage() {
       status="404"
       title="404"
       subTitle="Sorry, the page you visited does not exist."
-      extra={<Button type="primary">Back Home</Button>}
     />
   );
 }
