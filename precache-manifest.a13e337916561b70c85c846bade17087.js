b196069efcbc94e77eeab17a43faa980cc589819
caesiumy <mn0658@naver.com>
2020-06-14 15:55:44 +0900

    Updates

---
+++ precache-manifest.a13e337916561b70c85c846bade17087.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "735e554311fb93b9a6aaa55188728ae4",
+    "url": "/reactnd-would-you-rather/index.html"
+  },
+  {
+    "revision": "dd5b9caada1888185ddb",
+    "url": "/reactnd-would-you-rather/static/css/2.4c6ee7a4.chunk.css"
+  },
+  {
+    "revision": "343e57c57673a0147bb0",
+    "url": "/reactnd-would-you-rather/static/css/main.21306024.chunk.css"
+  },
+  {
+    "revision": "dd5b9caada1888185ddb",
+    "url": "/reactnd-would-you-rather/static/js/2.de7173c4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactnd-would-you-rather/static/js/2.de7173c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "343e57c57673a0147bb0",
+    "url": "/reactnd-would-you-rather/static/js/main.aff73a43.chunk.js"
+  },
+  {
+    "revision": "64ca204376298221c077",
+    "url": "/reactnd-would-you-rather/static/js/runtime-main.f8ac89af.js"
+  }
+]);
